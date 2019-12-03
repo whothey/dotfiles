312291323023f2aaccfd107cbc09eed3279ce113
Gabriel Henrique Rudey <ucraaa@gmail.com>
2019-12-02 23:14:18 -0300

    new lock image and script refactoring

--- custom_scripts/.scripts/lockscript.sh
+++ custom_scripts/.scripts/lockscript.sh
@@ -1,17 +1,10 @@
 #!/bin/zsh -e
 
-# Take a screenshot
-grim /tmp/screen_locked.png
-
-convert /tmp/screen_locked.png -colorspace Gray /tmp/screen_locked
-
-# Pixellate it 10x
-mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-# Blurry image
-#mogrify -gaussian-blur 10x10 /tmp/screen_locked.png
+# Take a screenshot and blurrify image
+grim - | convert - \
+  -blur 0x8 \
+  -colorspace Gray \
+  /tmp/screen_locked.png
 
 # Lock screen displaying this image.
-swaylock -i /tmp/screen_locked.png -k -l
-
-# Turn the screen off after a delay.
-#sleep 60; pgrep i3lock && xset dpms force off
+swaylock -kli /tmp/screen_locked.png && rm /tmp/screen_locked.png
