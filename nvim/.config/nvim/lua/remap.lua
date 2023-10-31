1b896d84cb1559b5703eb1def11985b6800f2e7d
Gabriel Henrique Rudey <ucraaa@gmail.com>
2023-10-31 20:52:31 -0300

    refactor(nvim): better lsp keymapings

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end)
+vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
