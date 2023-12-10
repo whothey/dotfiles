4a0cd6a6bbf956115abbd82785abd6b767e1f19f
Gabriel Henrique Rudey <ucraaa@gmail.com>
2023-12-09 22:41:49 -0300

    feat(nvim): telescope dap and new bindings

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,8 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').load_extension('dap')
+
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
@@ -11,9 +14,9 @@ vim.keymap.set('n', '<leader>pg', function()
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>tp', builtin.pickers, {})
-vim.keymap.set('n', '<C-S-o>', builtin.lsp_document_symbols, {})
 
 -- LSP
+vim.keymap.set('n', '<leader>lss', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>pS', builtin.lsp_workspace_symbols, {})
 vim.keymap.set('n', '<leader>lsr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>lsi', builtin.lsp_implementations, {})
