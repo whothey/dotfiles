caaa77decbec2386dae835c46e0be488eb45d53f
Gabriel Henrique Rudey <ucraaa@gmail.com>
2024-01-17 13:57:50 -0300

    feat(nvim): go to definition on a new window

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -37,6 +37,24 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gDh", function()
+      vim.cmd('vsplit');
+      vim.lsp.buf.definition();
+    end, opts)
+    vim.keymap.set("n", "gDl", function()
+      vim.cmd('vsplit');
+      vim.cmd('wincmd l');
+      vim.lsp.buf.definition();
+    end, opts)
+    vim.keymap.set("n", "gDk", function()
+      vim.cmd('split');
+      vim.lsp.buf.definition();
+    end, opts)
+    vim.keymap.set("n", "gDj", function()
+      vim.cmd('split');
+      vim.cmd('wincmd j');
+      vim.lsp.buf.definition();
+    end, opts)
     vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
