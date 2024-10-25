9f7db25e6cd6cd1c9515339682cc667cecab6639
Gabriel Henrique Rudey <ucraaa@gmail.com>
2024-10-25 14:05:17 -0300

    feat(nvim): add formatting keymap for LSP

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -79,6 +79,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>lrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>lsf", function() vim.lsp.buf.formatting() end, opts)
     vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
   end
