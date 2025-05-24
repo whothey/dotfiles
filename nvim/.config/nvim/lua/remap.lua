17caa7847cc0169d8ec64f454e96885be8f08019
Gabriel Henrique Rudey <ucraaa@gmail.com>
2025-05-24 18:44:03 -0300

    feat(nvim): better window nav

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -1,9 +1,11 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<C-c>", "<Esc>")
-vim.keymap.set("n", "<C-h>", "<C-W>h")
-vim.keymap.set("n", "<C-j>", "<C-W>j")
-vim.keymap.set("n", "<C-k>", "<C-W>k")
-vim.keymap.set("n", "<C-l>", "<C-W>l")
+vim.keymap.set("n", "<C-c>", "<Esc>", { desc = "Exit by C-c" })
+vim.keymap.set("n", "<C-h>", "<C-W>h", { desc = "Move to window on left" })
+vim.keymap.set("n", "<C-j>", "<C-W>j", { desc = "Move to window below" })
+vim.keymap.set("n", "<C-k>", "<C-W>k", { desc = "Move to window above" })
+vim.keymap.set("n", "<C-l>", "<C-W>l", { desc = "Move to window on right" })
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Next item on quickfix" })
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Prev item on quickfix" })
 vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, { desc = "Jump to next warning/error" } )
 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, { desc = "Jump to previous warning/error" })
 vim.keymap.set("n", "]t", function() vim.cmd('tabnext') end, { desc = "Next tab" })
@@ -12,5 +14,6 @@ vim.keymap.set("n", "]T", function() vim.cmd('tabm +1') end, { desc = "Move tab 
 vim.keymap.set("n", "[T", function() vim.cmd('tabm -1') end, { desc = "Move tab left" })
 vim.keymap.set("n", "<leader>tc", function() vim.cmd('tabnew') end, { desc = "Create a tab" })
 vim.keymap.set("n", "<leader>td", function() vim.cmd('tabclose') end, { desc = "Close a tab" })
+vim.keymap.set("n", "<leader>do", function() vim.diagnostic.open_float() end, { desc = "Open diagnostics float" });
 vim.keymap.set("n", "<leader>do", function() vim.diagnostic.open_float() end, { desc = "Open diagnostics float" });
 vim.keymap.set("n", "<leader>dt", function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end, { desc = "Toggle diagnostics" });
